30eedd472a0b0c01c0bab682d8068108d3eada60
michaellrodc <michaellrodc@gmail.com>
2023-05-22 01:05:15 -0500

    Clases Comision y Descuento creadas

---
+++ src/proyectosoftware/Comision.java
@@ -0,0 +1,23 @@
+
+package proyectosoftware;
+
+public class Comision {
+
+    private int horasExtraAntes;
+    private int horasExtraDespues;
+
+    public Comision(int horasExtraAntes, int horasExtraDespues) {
+        this.horasExtraAntes = horasExtraAntes;
+        this.horasExtraDespues = horasExtraDespues;
+    }
+    
+    public float calcularComision()
+    {
+    
+        return 0;
+    }
+    
+    
+    
+    
+}
---
+++ src/proyectosoftware/Descuento.java
@@ -0,0 +1,19 @@
+
+package proyectosoftware;
+
+public class Descuento {
+    private float aportacionesIESS;
+    private float aportacionesSRI;
+
+    public Descuento(float aportacionesIESS, float aportacionesSRI) {
+        this.aportacionesIESS = aportacionesIESS;
+        this.aportacionesSRI = aportacionesSRI;
+    }
+    
+    public float calcularDescuento()
+    {
+        
+        return 0;
+    }
+    
+}
