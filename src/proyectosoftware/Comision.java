f31198ac01c6487951a2bdb30535209816e39339
michaellrodc <michaellrodc@gmail.com>
2023-05-23 00:59:21 -0500

    Comision hora si

--- src/proyectosoftware/Comision.java
+++ src/proyectosoftware/Comision.java
@@ -1,23 +1,51 @@
-
 package proyectosoftware;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
 
 public class Comision {
 
     private int horasExtraAntes;
     private int horasExtraDespues;
+    private float valorComision;
 
-    public Comision(int horasExtraAntes, int horasExtraDespues) {
-        this.horasExtraAntes = horasExtraAntes;
-        this.horasExtraDespues = horasExtraDespues;
+    public Comision() {
+    this.horasExtraAntes = 0;
+    this.horasExtraDespues = 0;
+    this.valorComision = 0;
     }
-    
-    public float calcularComision()
+    public float calcularComision(String codigo) throws SQLException
     {
-    
-        return 0;
+        Conexion conexion = new Conexion();
+        Connection con = null;
+        PreparedStatement stmt = null;
+        ResultSet result = null;
+        float comisionTotal=0;
+        try {
+            con = conexion.conector();
+            String query = "SELECT c.horasExtraAntes, c.horasExtraDespues, c.valorComision, e.emp_salario FROM empleado e, comision c, salario s WHERE c.com_codigo = ? AND c.com_codigo = s.slr_cms_codigo AND e.emp_sal_codigo = s.slr_codigo";
+            stmt = con.prepareStatement(query);
+
+            stmt.setString(1, codigo);
+            
+            horasExtraAntes = result.getInt("horasExtraAntes");
+            horasExtraDespues = result.getInt("horasExtraDespues");
+            valorComision = result.getFloat("valorComision");
+            float salario = result.getFloat("emp_salario");
+            result = stmt.executeQuery();
+            
+            
+            comisionTotal = (float) (((horasExtraAntes * 1.5)*salario/160)+((horasExtraDespues * 2)*salario/160));
+        } catch (IOException | SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error en " + ex.getMessage());
+            }
+        return comisionTotal;
+              
+        }
+          
+        
     }
-    
-    
-    
-    
-}
--- src/proyectosoftware/pantallaConsultar.java
+++ src/proyectosoftware/pantallaConsultar.java
@@ -60,6 +60,11 @@ public class pantallaConsultar extends javax.swing.JFrame {
         txtCodigo.setText("jTextField1");
 
         jButton1.setText("Calcular");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -69,7 +74,7 @@ public class pantallaConsultar extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(57, 57, 57)
                 .addComponent(txtCodigo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jButton1)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
@@ -98,6 +103,10 @@ public class pantallaConsultar extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
