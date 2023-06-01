d6e5c16e8c99bc332dd245be0fa4d0e460a440cc
MaoAstud <maoastudillo3@gmail.com>
2023-05-31 19:33:02 -0500

    Se añadio la funcion get empleado
    La funcion empleado permitira traer cualquier cliente y todos sus atributos de la base de datos a traves de su cedula.

--- src/proyectosoftware/Empleado.java
+++ src/proyectosoftware/Empleado.java
@@ -2,6 +2,7 @@ package proyectosoftware;
 
 import java.io.IOException;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
@@ -10,9 +11,10 @@ import javax.swing.JOptionPane;
  * @author mao_a
  */
 public class Empleado {
-    Conexion conexion = new Conexion();
-    java.sql.Connection con = null;
-    PreparedStatement stmt = null;
+    private static Conexion conexion = new Conexion();
+    private static java.sql.Connection con = null;
+    private static PreparedStatement stmt = null;
+    private static ResultSet result = null;
     
     private String cedula;
     private String nombre;
@@ -22,10 +24,40 @@ public class Empleado {
     private int horasEquivalentes;
     private double salarioNeto;
     private String codigoSalario;
+    
+    
+    public String getCedula() {
+        return cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public String getContrato() {
+        return contrato;
+    }
+
+    public int getHorasEquivalentes() {
+        return horasEquivalentes;
+    }
 
     public double getSalarioNeto() {
         return salarioNeto;
     }
+
+    public String getCodigoSalario() {
+        return codigoSalario;
+    }
+    
 
     public Empleado(String cedula, String nombre, String apellido, String categoria, String contrato, int horasEquivalentes, double salarioNeto) {
         this.cedula = cedula;
@@ -39,6 +71,39 @@ public class Empleado {
 
     }
     
+    
+    static public Empleado getEmpleado(String cedula) {
+        Empleado empleado = null;
+        
+        try {
+            con = conexion.conector();
+            
+            String cadena = "SELECT * FROM Empleado WHERE Empleado.emp_cedula = ?";
+            stmt = con.prepareStatement(cadena);
+            stmt.setString(1, cedula);
+            result = stmt.executeQuery();
+            
+            empleado = new Empleado(
+                    result.getString("emp_cedula"),
+                    result.getString("emp_nombre"),
+                    result.getString("emp_apellido"),
+                    result.getString("emp_categoria"),
+                    result.getString("emp_contrato"),
+                    result.getInt("emp_horasEquivalentes"),
+                    result.getDouble("emp_salarioNeto")
+            );
+            
+            stmt.close();
+            con.close();
+            
+        } catch (IOException | SQLException ex) {
+            System.out.println("Error en " + ex.getMessage());
+        }
+        
+        return empleado;
+    }
+    
+
     public boolean registroEmpleado(){
         try {
             con = conexion.conector();
